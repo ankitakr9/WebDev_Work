f96aee100d1230171c9f8280f1ab89e6b87a42fc
Ankita Kumari <113806915+ankitakr9@users.noreply.github.com>
2025-08-03 06:41:09 +0530

    Add files via upload

---
+++ PracticeJS/popcorn.js
@@ -0,0 +1,17 @@
+let size = "XL";
+if(size === "XL"){
+    console.log("Price of Popcorn is 250");
+}
+else if(size === 'L'){
+    console.log("Price of popcorn is 200");
+
+}
+else if(size === 'M'){
+    console.log("Price of popcorn is 100");
+}
+else if(size === 'S'){
+    console.log("Price of popcorn is 50");
+}
+else{
+    console.log(" Not provided for now any more sizes");
+}
---
+++ PracticeJS/trafic.js
@@ -0,0 +1,18 @@
+
+//Traffic Light System
+let color = "blue"
+if(color === "red"){
+    console.log("Stop! - light color is red");
+
+}
+else if(color === "yellow"){
+    console.log("Slow Down - light color is yellow");
+
+}
+else if(color === "green"){
+    console.log("you can go.. light color is green");
+}
+else
+    {
+        console.log("Traffic light is broken ");
+}
